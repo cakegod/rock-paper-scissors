3147e52227fc82c5dec2e179877079aab870803c
Cake <cakemancerr@gmail.com>
2021-05-31 13:06:43 +0200

    Improved playRound function

--- script.js
+++ script.js
@@ -24,37 +24,25 @@ if (rounds === 5 && playerScore == computerScore) {
     alert("DRAW!");
 } else if (rounds === 5 && playerScore > computerScore) {
     alert("WIN!");
-} else if (rounds === 5 && playerScore < computerScore) 
+} else if (rounds === 5 && playerScore < computerScore)
     alert("GAME OVER!");
 
- // Plays one round
+// Plays one round
 function playRound(computerSelection, playerSelection) {
     if (playerSelection == computerSelection) {
         alert("It's a draw!");
         alert("Player's score " + playerScore + " Computer's score " + computerScore);
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
+    } else if ((playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "rock" && computerSelection == "scissors")) {
         playerScore++;
-        alert("You win! Paper beats rock");
+        alert("You win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + ".");
         alert("Player's score " + playerScore + " Computer's score " + computerScore);
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    } else if ((playerSelection == "scissors" && computerSelection == "rock") ||
+        (playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "scissors")) {
         computerScore++;
-        alert("You lose! Rock beats Scissors");
-        alert("Player's score " + playerScore + " Computer's score " + computerScore);
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        playerScore++;
-        alert("You win! Scissors beats paper");
-        alert("Player's score " + playerScore + " Computer's score " + computerScore);
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        computerScore++;
-        alert("You lose! Paper beats rock");
-        alert("Player's score " + playerScore + " Computer's score " + computerScore);
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        playerScore++;
-        alert("You win! Rock beats Scissors");
-        alert("Player's score " + playerScore + " Computer's score " + computerScore);
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        computerScore++;
-        alert("You lose! Scissors beats paper");
+        alert("You lose! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + ".");
         alert("Player's score " + playerScore + " Computer's score " + computerScore);
 
     }
